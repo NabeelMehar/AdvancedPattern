6986212f897cfb5340181ee202597cb26ff3b90a
NabeelMehar <nabeel.mehar.khan@gmail.com>
2020-03-20 13:50:46 +0530

    Finished Pattern

--- src/AdvancedPattern1.java
+++ src/AdvancedPattern1.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class AdvancedPattern1 {
     public static void main(String[] args) {
-        
-        Scanner scanner=new Scanner(System.in);
-        int n=scanner.nextInt();
 
-        int size=0;
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count=1;
 
-        while (size!=n) {
-            for (int i = 1; i <=(2*((n-size)-1)); i++) {
+        int size = 0;
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i <= (2 * ((n - size) - 1)); i++) {
                 System.out.print(" ");
             }
-            for (int i = 1; i <=size; i++) {
-                System.out.print("*  ");
+            for (int i = 0; i <= size; i++) {
+                System.out.print(count+"   ");
+                count++;
             }
             System.out.println();
             size++;
