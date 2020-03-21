3cc39a323e6d34c156680baf8457b1331354da30
NabeelMehar <nabeel.mehar.khan@gmail.com>
2020-03-21 21:15:58 +0530

    Sorting Started

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -3,5 +3,32 @@ package sorting;
 public class SelectionSort {
     public static void main(String[] args) {
 
+        int[] arr={9,8,7,10,12,3,14,2,1};
+        int smallest=0;
+        int preSmallest=0;
+        int tempr=arr.length;
+
+
+        while (smallest!=arr.length) {
+
+
+            for (int i = preSmallest; i < arr.length; i++) {
+                if (arr[smallest] > arr[i]) {
+                    smallest = i;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[preSmallest];
+            arr[preSmallest] = temp;
+
+            preSmallest++;
+            smallest = preSmallest;
+        }
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
     }
+
 }
